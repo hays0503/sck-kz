8eeb19699bea3332f9c5d22eb5d170a752417ad0
hays0503 <cthtufgbv@mail.ru>
2025-02-12 01:33:15 +0500

    fix

--- src/features/login-with-google/api/createGoogleAccount.ts
+++ src/features/login-with-google/api/createGoogleAccount.ts
@@ -14,7 +14,9 @@ export type CreateGoogleAccount = {
 };
 
 const createGoogleAccount: CreateGoogleAccount = async (code) => {
-  return await fetch(`http://${process.env.API_URL}:${process.env.API_AUTH_PORT}/auth_api/v1/auth_user/auth/google?code=${code}`, {
+  const url = `http://${process.env.API_URL}:${process.env.API_AUTH_PORT}/auth_api/v1/auth_user/auth/google?code=${code}`;
+  console.log("createGoogleAccount url=>", url);
+  return await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
