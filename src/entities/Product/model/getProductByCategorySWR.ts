3958a478a2d2a393d0c328c73eba83b596515e65
hays0503 <cthtufgbv@mail.ru>
2025-03-18 05:14:21 +0500

    ff

--- src/entities/Product/model/getProductByCategorySWR.ts
+++ src/entities/Product/model/getProductByCategorySWR.ts
@@ -13,7 +13,8 @@ const useGetProductByCategorySWR = ({
   orderBy: "avg_rating" | "-avg_rating" | "stocks__price" | "-stocks__price";
   page: number;
 }): SWRResponse<getProductResult> => {
-
+  console.log("useGetProductByCategorySWR");
+  console.count("useGetProductByCategorySWR = >>");
   const url = `/api-mapping/product/by_category/?category=${category}&order=${orderBy}&page=${page}&city=${city}`;
 
   const object = useSWR(url, (_url) => defaultFetcher(_url));
