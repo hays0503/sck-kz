8090f91b2dbe6bfbd8e9e9a9bdfe68ed6c2a5706
hays0503 <cthtufgbv@mail.ru>
2025-03-28 01:50:24 +0500

    new user info for sentry

--- src/shared/providers/providersClient.tsx
+++ src/shared/providers/providersClient.tsx
@@ -1,12 +1,14 @@
-"use client";
+'use client';
 
-import { SWRConfig } from "swr";
-import { NuqsAdapter } from "nuqs/adapters/next";
-import { Suspense, useEffect } from "react";
+import { SWRConfig } from 'swr';
+import { NuqsAdapter } from 'nuqs/adapters/next';
+import { Suspense, useEffect } from 'react';
 import '@ant-design/v5-patch-for-react-19';
-import { useLocalStorage, useReadLocalStorage } from "@undefined/usehooks-ts";
-import timeCalculate from "@/entities/User/model/timeCalculate";
-import { verifyAuth } from "@/entities/User/model/useIsAnonymous";
+import { useLocalStorage, useReadLocalStorage } from '@undefined/usehooks-ts';
+import timeCalculate from '@/entities/User/model/timeCalculate';
+import { verifyAuth } from '@/entities/User/model/useIsAnonymous';
+import { getUserInfo } from '@/entities/User';
+import * as Sentry from '@sentry/react';
 
 export function ProvidersClient({
   children,
@@ -18,29 +20,70 @@ export function ProvidersClient({
     [key: string]: any;
   };
 }) {
-  const [AccessToken, setAccessToken] = useLocalStorage<{
-    expires_at: Date | undefined;
-    issued_at: Date | undefined;
-    token: string | undefined;
-  } | undefined>("accessToken", { expires_at: undefined, token: undefined, issued_at: undefined });
+  const [AccessToken, setAccessToken] = useLocalStorage<
+    | {
+        expires_at: Date | undefined;
+        issued_at: Date | undefined;
+        token: string | undefined;
+      }
+    | undefined
+  >('accessToken', {
+    expires_at: undefined,
+    token: undefined,
+    issued_at: undefined,
+  });
 
-  const RefreshToken = useReadLocalStorage<{ token: string | undefined }>("refreshToken");
+  const RefreshToken = useReadLocalStorage<{ token: string | undefined }>(
+    'refreshToken',
+  );
 
   useEffect(() => {
     if (AccessToken?.expires_at && AccessToken?.issued_at) {
-      if(timeCalculate(new Date(AccessToken?.expires_at))){
-        if(RefreshToken?.token){
+      if (timeCalculate(new Date(AccessToken?.expires_at))) {
+        if (RefreshToken?.token) {
           verifyAuth(RefreshToken?.token).then((response) => {
             if (response.isVerified && response.data) {
               setAccessToken(response.data?.access);
+              getUserInfo(response.data?.access.token).then((response) => {
+                if (response.statusCode === 200) {
+                  if (response.data) {
+                    navigator.geolocation.getCurrentPosition((position) => {
+                      const positionUrl = `https://www.google.com/maps/search/?api=1&query=${position.coords.latitude},${position.coords.longitude}`
+                      console.log("positionUrl =>",positionUrl);
+                      Sentry.setUser({
+                        id: response.data?.id ?? 'No id',
+                        email: response.data?.email ?? 'No email',
+                        username: response.data?.username ?? 'No username',
+                        addressUrl: positionUrl,
+                      });
+                    }, () => {
+                      console.log("No position");
+                      Sentry.setUser({
+                        id: response.data?.id ?? 'No id',
+                        email: response.data?.email ?? 'No email',
+                        username: response.data?.username ?? 'No username',
+                        addressUrl: 'No position',
+                      });
+                    },{
+                      enableHighAccuracy: true,
+                      timeout: 5000,
+                      maximumAge: 0,
+                    });
+                  }
+                }
+              });
             }
-          })
+          });
         }
       }
     }
     return () => {};
-  
-  }, [AccessToken?.expires_at, AccessToken?.issued_at,RefreshToken?.token, setAccessToken]);
+  }, [
+    AccessToken?.expires_at,
+    AccessToken?.issued_at,
+    RefreshToken?.token,
+    setAccessToken,
+  ]);
 
   return (
     <SWRConfig value={{ fallback }}>
