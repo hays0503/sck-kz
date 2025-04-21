3bce9592d201ddb0ff15d1b01a6c5e3c01ba21ff
hays0503 <cthtufgbv@mail.ru>
2025-04-21 15:39:42 +0500

    Изменения в типах в файлах интернационализации

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,14 +1,13 @@
- 
-import {getRequestConfig} from 'next-intl/server';
+ import {getRequestConfig} from 'next-intl/server';
 import {hasLocale} from 'next-intl';
 import {routing} from './routing';
- 
+
 export default getRequestConfig(async ({requestLocale}) => {
   const requested = await requestLocale;
+  // Гарантируем, что locale всегда будет строкой
   const locale = hasLocale(routing.locales, requested)
     ? requested
-    : routing.defaultLocale;
- 
+    : routing.defaultLocale || 'ru'; // Устанавливаем 'ru' как запасной вариант
 
   return {
     locale,
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -13,6 +13,6 @@ export const routing = defineRouting({
  
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter} =
+export const {Link, redirect, usePathname, useRouter,getPathname} =
 createNavigation(routing);
 
