0ae355ef7544cd442f2a3796d731a1449f7bba02
hays0503 <cthtufgbv@mail.ru>
2025-05-12 14:06:21 +0500

    fix

--- app/api-mapping/category/root/mapping/mapping.ts
+++ app/api-mapping/category/root/mapping/mapping.ts
@@ -3,6 +3,7 @@ import { MappedCategoryWithoutChildrenType, rawTypeCategory } from "../type";
 const mapping = (rawData: rawTypeCategory[]|[]):{results:MappedCategoryWithoutChildrenType[]} => {
     if(rawData.length === 0) return {results: []};
     const category = rawData.map((product: rawTypeCategory) => ({
+        id: product.id,
         slug: product.slug,
         name: {
             ru: product.name_category,
